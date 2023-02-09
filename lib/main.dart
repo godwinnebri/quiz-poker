a064cd48ecf222d82866c1d70ff4088cc4eaa9de
Godwin Ebri <104990430+godwinnebri@users.noreply.github.com>
2023-02-09 22:25:42 +0100

    feat: updated question listview

--- lib/main.dart
+++ lib/main.dart
@@ -12,32 +12,121 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       theme: ThemeData(
           primarySwatch: Colors.blue,
-          scaffoldBackgroundColor: Color(0xffF8FBFF)),
-      home: const HomePage(),
+          scaffoldBackgroundColor: const Color(0xffF8FBFF)),
+      home: HomePage(),
     );
   }
 }
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  HomePage({super.key});
+
+  final List<Map> questions = [
+    {
+      'number': 1,
+      'category': 'Physics',
+      'image':
+          'https://assets.ltkcontent.com/images/26593/physics_27c5571306.jpg',
+      'question':
+          'What is the law that states that for every action, there is an equal and opposite reaction?',
+    },
+    {
+      'number': 2,
+      'category': 'Geography',
+      'image':
+          'https://www.redeemer.ca/wp-content/uploads/program-geography-social-share.jpg',
+      'question': 'What planet is known as the Red Planet?',
+    },
+    {
+      'number': 3,
+      'category': 'Art',
+      'image':
+          'https://guardian.ng/wp-content/uploads/2022/12/African-art-scaled.jpeg',
+      'question': 'Who painted the famous artwork, the Mona Lisa?',
+    },
+    {
+      'number': 4,
+      'category': 'Literature',
+      'image':
+          'https://nationaltoday.com/wp-content/uploads/2022/06/4-Literature.jpg',
+      'question': 'Who wrote the play Romeo and Juliet?',
+    },
+    {
+      'number': 5,
+      'category': 'History',
+      'image':
+          'https://i.natgeofe.com/k/1bc6572d-cb59-48b4-8d95-3e3a0f3c1b8a/history-og_16x9.jpg?w=1200',
+      'question': 'What is the capital of France?',
+    },
+    {
+      'number': 6,
+      'category': 'Literature',
+      'image':
+          'https://nationaltoday.com/wp-content/uploads/2022/06/4-Literature.jpg',
+      'question':
+          'Who is the main character in the book To Kill a Mockingbird?',
+    },
+    {
+      'number': 7,
+      'category': 'Geography',
+      'image':
+          'https://www.redeemer.ca/wp-content/uploads/program-geography-social-share.jpg',
+      'question': 'What is the largest ocean on Earth?',
+    },
+    {
+      'number': 8,
+      'category': 'History',
+      'image':
+          'https://i.natgeofe.com/k/1bc6572d-cb59-48b4-8d95-3e3a0f3c1b8a/history-og_16x9.jpg?w=1200',
+      'question': 'Who discovered America?',
+    },
+    {
+      'number': 9,
+      'category': 'Geography',
+      'image':
+          'https://www.redeemer.ca/wp-content/uploads/program-geography-social-share.jpg',
+      'question': 'What is the currency used in Japan?',
+    },
+    {
+      'number': 10,
+      'category': 'Art',
+      'image':
+          'https://guardian.ng/wp-content/uploads/2022/12/African-art-scaled.jpeg',
+      'question': 'Who composed the opera The Barber of Seville"',
+    },
+    {
+      'number': 11,
+      'category': 'Geography',
+      'image':
+          'https://www.redeemer.ca/wp-content/uploads/program-geography-social-share.jpg',
+      'question': 'What is the highest mountain in the solar system?',
+    },
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
+          backgroundColor: Colors.white,
+          foregroundColor: Colors.blue,
+          elevation: 0,
           title: const Text('Quiz poker'),
         ),
         body: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 20),
-          child: Column(
-            children: [
-              QuestionContainer(
-                question:
-                    'What is the law that states that for every action, there is an equal and opposite reaction?',
-                questionCategory: 'Physics',
-                questionNumnber: 1,
-              ),
-            ],
+          padding: const EdgeInsets.symmetric(horizontal: 18, vertical: 20),
+          child: ListView.separated(
+            padding: const EdgeInsets.all(0),
+            scrollDirection: Axis.vertical,
+            separatorBuilder: (context, index) => const SizedBox(height: 12),
+            itemCount: questions.length,
+            itemBuilder: (BuildContext context, int index) {
+              return QuestionContainer(
+                question: questions[index]['question'],
+                questionCategory: questions[index]['category'],
+                questionNumnber: questions[index]['number'],
+                image: questions[index]['image'],
+              );
+            },
           ),
         ),
         floatingActionButton: FloatingActionButton(
@@ -59,15 +148,17 @@ class HomePage extends StatelessWidget {
 }
 
 class QuestionContainer extends StatelessWidget {
-  QuestionContainer(
+  const QuestionContainer(
       {super.key,
       required this.question,
       required this.questionCategory,
-      required this.questionNumnber});
+      required this.questionNumnber,
+      required this.image});
 
-  int questionNumnber;
-  String questionCategory;
-  String question;
+  final int questionNumnber;
+  final String questionCategory;
+  final String question;
+  final String image;
 
   @override
   Widget build(BuildContext context) {
@@ -79,25 +170,44 @@ class QuestionContainer extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(
-              'Question $questionNumnber',
-              style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
-            ),
-            const SizedBox(height: 8),
-            Container(
-              decoration: BoxDecoration(
-                  color: Colors.grey.withOpacity(0.2),
-                  borderRadius: BorderRadius.circular(20)),
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(
-                  questionCategory,
-                  style: const TextStyle(fontSize: 12),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Question $questionNumnber',
+                  style: const TextStyle(
+                      fontWeight: FontWeight.bold, fontSize: 16),
                 ),
-              ),
+                Container(
+                  decoration: BoxDecoration(
+                      color: Colors.blue.withOpacity(0.1),
+                      borderRadius: BorderRadius.circular(20)),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      questionCategory,
+                      style: const TextStyle(fontSize: 13, color: Colors.blue),
+                    ),
+                  ),
+                ),
+              ],
             ),
-            const SizedBox(height: 8),
-            Text(question),
+            const SizedBox(height: 12),
+            Row(
+              children: [
+                Container(
+                  height: 60,
+                  width: 60,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8),
+                    image: DecorationImage(
+                        image: NetworkImage(image), fit: BoxFit.cover),
+                  ),
+                ),
+                const SizedBox(width: 14),
+                Flexible(child: Text(question)),
+              ],
+            ),
           ],
         ),
       ),
