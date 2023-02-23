65918255ebbfe610b69e0c3fc9ab445aface388e
Godwin Ebri <104990430+godwinnebri@users.noreply.github.com>
2023-02-24 00:54:17 +0100

    code refactor

--- lib/main.dart
+++ lib/main.dart
@@ -13,104 +13,71 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
           primarySwatch: Colors.blue,
           scaffoldBackgroundColor: const Color(0xffF8FBFF)),
-      home: HomePage(),
+      home: const HomePage(),
     );
   }
 }
 
 class HomePage extends StatelessWidget {
-  HomePage({super.key});
-
-  final List<Map> questions = [
-    {
-      'number': 1,
-      'category': 'Physics',
-      'image':
-          'https://assets.ltkcontent.com/images/26593/physics_27c5571306.jpg',
-      'question':
-          'What is the law that states that for every action, there is an equal and opposite reaction?',
-    },
-    {
-      'number': 2,
-      'category': 'Geography',
-      'image':
-          'https://www.redeemer.ca/wp-content/uploads/program-geography-social-share.jpg',
-      'question': 'What planet is known as the Red Planet?',
-    },
-    {
-      'number': 3,
-      'category': 'Art',
-      'image':
-          'https://guardian.ng/wp-content/uploads/2022/12/African-art-scaled.jpeg',
-      'question': 'Who painted the famous artwork, the Mona Lisa?',
-    },
-    {
-      'number': 4,
-      'category': 'Literature',
-      'image':
-          'https://nationaltoday.com/wp-content/uploads/2022/06/4-Literature.jpg',
-      'question': 'Who wrote the play Romeo and Juliet?',
-    },
-    {
-      'number': 5,
-      'category': 'History',
-      'image':
-          'https://i.natgeofe.com/k/1bc6572d-cb59-48b4-8d95-3e3a0f3c1b8a/history-og_16x9.jpg?w=1200',
-      'question': 'What is the capital of France?',
-    },
-    {
-      'number': 6,
-      'category': 'Literature',
-      'image':
-          'https://nationaltoday.com/wp-content/uploads/2022/06/4-Literature.jpg',
-      'question':
-          'Who is the main character in the book To Kill a Mockingbird?',
-    },
-    {
-      'number': 7,
-      'category': 'Geography',
-      'image':
-          'https://www.redeemer.ca/wp-content/uploads/program-geography-social-share.jpg',
-      'question': 'What is the largest ocean on Earth?',
-    },
-    {
-      'number': 8,
-      'category': 'History',
-      'image':
-          'https://i.natgeofe.com/k/1bc6572d-cb59-48b4-8d95-3e3a0f3c1b8a/history-og_16x9.jpg?w=1200',
-      'question': 'Who discovered America?',
-    },
-    {
-      'number': 9,
-      'category': 'Geography',
-      'image':
-          'https://www.redeemer.ca/wp-content/uploads/program-geography-social-share.jpg',
-      'question': 'What is the currency used in Japan?',
-    },
-    {
-      'number': 10,
-      'category': 'Art',
-      'image':
-          'https://guardian.ng/wp-content/uploads/2022/12/African-art-scaled.jpeg',
-      'question': 'Who composed the opera The Barber of Seville"',
-    },
-    {
-      'number': 11,
-      'category': 'Geography',
-      'image':
-          'https://www.redeemer.ca/wp-content/uploads/program-geography-social-share.jpg',
-      'question': 'What is the highest mountain in the solar system?',
-    },
-  ];
+  const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    double sizee = MediaQuery.of(context).size.width;
+    final List<Question> questions = [
+      Question(
+        category: QuestionCategory.physics,
+        questionText:
+            'What is the law that states that for every action, there is an equal and opposite reaction?',
+      ),
+      Question(
+        category: QuestionCategory.geography,
+        questionText: 'What planet is known as the Red Planet?',
+      ),
+      Question(
+        category: QuestionCategory.art,
+        questionText: 'Who painted the famous artwork, the Mona Lisa?',
+      ),
+      Question(
+        category: QuestionCategory.literature,
+        questionText: 'Who wrote the play Romeo and Juliet?',
+      ),
+      Question(
+        category: QuestionCategory.geography,
+        questionText: 'What is the capital of France?',
+      ),
+      Question(
+        category: QuestionCategory.literature,
+        questionText:
+            'Who is the main character in the book To Kill a Mockingbird?',
+      ),
+      Question(
+        category: QuestionCategory.geography,
+        questionText: 'What is the largest ocean on Earth?',
+      ),
+      Question(
+        category: QuestionCategory.history,
+        questionText: 'Who discovered America?',
+      ),
+      Question(
+        category: QuestionCategory.history,
+        questionText: 'What is the currency used in Japan?',
+      ),
+      Question(
+        category: QuestionCategory.literature,
+        questionText: 'Who composed the opera The Barber of Seville',
+      ),
+      Question(
+        category: QuestionCategory.geography,
+        questionText: 'What is the highest mountain in the solar system?',
+      ),
+    ];
     return Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.white,
           foregroundColor: Colors.blue,
           elevation: 0,
-          title: const Text('Quiz poker'),
+          title: Text('Quiz poker $sizee'),
         ),
         body: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 18, vertical: 20),
@@ -118,16 +85,15 @@ class HomePage extends StatelessWidget {
             return ListView.separated(
               padding: const EdgeInsets.all(0),
               scrollDirection:
-                  constraints.maxWidth > 393 ? Axis.horizontal : Axis.vertical,
+                  constraints.maxWidth > 430 ? Axis.horizontal : Axis.vertical,
               separatorBuilder: (context, index) =>
                   const SizedBox(height: 12, width: 12),
               itemCount: questions.length,
               itemBuilder: (BuildContext context, int index) {
                 return QuestionContainer(
-                  question: questions[index]['question'],
-                  questionCategory: questions[index]['category'],
-                  questionNumnber: questions[index]['number'],
-                  image: questions[index]['image'],
+                  question: questions[index].questionText,
+                  questionCategory: questions[index].category,
+                  questionNumnber: 1 + index,
                 );
               },
             );
@@ -157,16 +123,17 @@ class QuestionContainer extends StatelessWidget {
     required this.question,
     required this.questionCategory,
     required this.questionNumnber,
-    required this.image,
   });
 
   final int questionNumnber;
-  final String questionCategory;
+  final questionCategory;
   final String question;
-  final String image;
+  // final String image;
 
   @override
   Widget build(BuildContext context) {
+    Question questionn =
+        Question(category: questionCategory, questionText: question);
     return Container(
       width: 393,
       decoration: BoxDecoration(
@@ -194,7 +161,7 @@ class QuestionContainer extends StatelessWidget {
                     padding: const EdgeInsets.all(8.0),
                     child: Center(
                       child: Text(
-                        questionCategory,
+                        questionCategory.toString(),
                         maxLines: 1,
                         overflow: TextOverflow.ellipsis,
                         style:
@@ -214,7 +181,8 @@ class QuestionContainer extends StatelessWidget {
                   decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(8),
                     image: DecorationImage(
-                        image: NetworkImage(image), fit: BoxFit.cover),
+                        image: NetworkImage(questionn.category.getImage()),
+                        fit: BoxFit.cover),
                   ),
                 ),
                 const SizedBox(width: 14),
@@ -226,4 +194,43 @@ class QuestionContainer extends StatelessWidget {
       ),
     );
   }
+}
+
+enum QuestionCategory {
+  physics,
+  geography,
+  art,
+  literature,
+  history;
+
+  @override
+  String toString() => toTitleCase(name);
+  String getImage() {
+    switch (this) {
+      case QuestionCategory.art:
+        return 'https://guardian.ng/wp-content/uploads/2022/12/African-art-scaled.jpeg';
+      case QuestionCategory.geography:
+        return 'https://www.redeemer.ca/wp-content/uploads/program-geography-social-share.jpg';
+      case QuestionCategory.history:
+        return 'https://i.natgeofe.com/k/1bc6572d-cb59-48b4-8d95-3e3a0f3c1b8a/history-og_16x9.jpg?w=1200';
+      case QuestionCategory.literature:
+        return 'https://nationaltoday.com/wp-content/uploads/2022/06/4-Literature.jpg';
+      case QuestionCategory.physics:
+        return 'https://assets.ltkcontent.com/images/26593/physics_27c5571306.jpg';
+      default:
+        return '';
+    }
+  }
+}
+
+String toTitleCase(String name) => name[0].toUpperCase() + name.substring(1);
+
+class Question {
+  Question({
+    required this.category,
+    required this.questionText,
+  });
+
+  final QuestionCategory category;
+  final String questionText;
 }
